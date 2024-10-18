1a078d286c0992ce73998e6dbc379db1da9a774c
Guro <Guro@github.com>
2024-10-19 02:59:37 +0400

    Designn of Header Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { Header } from "@/sections/Header";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <Header />
     </div>
   );
 }
